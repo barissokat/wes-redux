ec00f21a816d6b45944b631d1b589ff164b7fcf3
Barış Sokat <barissokat@outlook.com>
2021-12-21 13:46:29 +0300

    Add new components and Router
    - Add Main, PhotoGrid and Single components
    - Set up React Router

---
+++ client/components/PhotoGrid.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PhotoGrid = () => {
+    return (
+        <div className='photo-grid'>
+            I'm the Photo Grid
+        </div>
+    )
+}
+
+export default PhotoGrid;
---
+++ client/components/Single.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Single = () => {
+    return (
+        <div className='single-photo'>
+            I'm the Single
+        </div>
+    )
+}
+
+export default Single;
